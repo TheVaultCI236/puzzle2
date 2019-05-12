68125f4c3bebbc8220d2d9d12dc9f4fe7313373b
TheVaultCI236 <50559307+TheVaultCI236@users.noreply.github.com>
2019-05-13 00:16:01 +0100

    Add files via upload

---
+++ Puzzle2.java
@@ -0,0 +1,19 @@
+
+public class Puzzle2 {
+	
+
+	public static void main(String Args[]) {
+		
+		// create a boolean for security 
+		// with the name securityAccess
+		// and assign it to be true
+		
+		// Also create a variable to hold the 
+		// security code with the variable name
+		// being codeAccess. This number will be
+		// in pure number format so pick the correct
+		// variable for this one. 
+		
+	
+	}
+}
